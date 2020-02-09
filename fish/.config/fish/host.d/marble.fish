086f3696810d937a655aeae83614e0e7c0786f53
Romain Baugue <romain.baugue@elwinar.com>
2020-02-09 12:26:50 +0100

    fish: Marble env

--- fish/.config/fish/host.d/marble.fish
+++ fish/.config/fish/host.d/marble.fish
@@ -1,3 +1,7 @@
 set -x PASSWORD_STORE_DIR ~/passwords
 set -x GOPATH $HOME/.go
 add_to_path $GOPATH/bin
+
+set -x STO_STDCOMPOSEFILE  $HOME/development/synthesio/standard/docker-compose.yml
+set -x STO_STDPATH $HOME/development/synthesio/standard
+set -x STO_STDDIRNAME standard
