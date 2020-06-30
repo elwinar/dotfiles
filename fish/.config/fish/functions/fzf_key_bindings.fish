a0da03b51839ff62466d9e074bf54b987c3f73c8
Romain Baugue <romain.baugue@elwinar.com>
2020-06-30 22:09:14 +0200

    fish: use wayland on marble

---
+++ fish/.config/fish/functions/fzf_key_bindings.fish
@@ -0,0 +1,1 @@
+/home/elwinar/.fzf/shell/key-bindings.fish
--- fish/.config/fish/host.d/marble.fish
+++ fish/.config/fish/host.d/marble.fish
@@ -1,7 +1,10 @@
 set -x PASSWORD_STORE_DIR ~/passwords
 set -x GOPATH $HOME/.go
 add_to_path $GOPATH/bin
+set -x MOZ_ENABLE_WAYLAND 1
 
 set -x STO_STDCOMPOSEFILE  $HOME/development/synthesio/standard/docker-compose.yml
 set -x STO_STDPATH $HOME/development/synthesio/standard
 set -x STO_STDDIRNAME standard
+set -x AGENT_NAPOLEON_URL http://127.0.0.1
+set -x AGENT_DEVELOPMENT true
