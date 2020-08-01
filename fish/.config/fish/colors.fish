8df91979ffbb0cfaaf6c4202903f6b35d3a17045
Romain Baugue <romain.baugue@elwinar.com>
2020-08-01 19:24:36 +0200

    fish: remove fishfile

---
+++ fish/.config/fish/colors.fish
@@ -0,0 +1,28 @@
+# Gruvbox dark, soft by Dawid Kurek (dawikur@gmail.com), morhetz (https://github.com/morhetz/gruvbox)
+
+set -g fish_color_autosuggestion 665c54
+set -g fish_color_cancel -r
+set -g fish_color_command b8bb26
+set -g fish_color_comment fabd2f
+set -g fish_color_cwd green
+set -g fish_color_cwd_root red
+set -g fish_color_end d3869b
+set -g fish_color_error fb4934
+set -g fish_color_escape 8ec07c
+set -g fish_color_history_current --bold
+set -g fish_color_host normal
+set -g fish_color_match 83a598
+set -g fish_color_normal normal
+set -g fish_color_operator 83a598
+set -g fish_color_param d5c4a1
+set -g fish_color_quote fabd2f
+set -g fish_color_redirection d5c4a1
+set -g fish_color_search_match bryellow --background=brblack
+set -g fish_color_selection white --bold --background=brblack
+set -g fish_color_user brgreen
+set -g fish_color_valid_path --underline
+set -g fish_pager_color_completion normal
+set -g fish_pager_color_description B3A06D yellow
+set -g fish_pager_color_prefix white --bold --underline
+set -g fish_pager_color_progress brwhite --background=cyan
+
