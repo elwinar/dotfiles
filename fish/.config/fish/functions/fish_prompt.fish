641b914cfa7dcd858efd316bb2822b278570ab1a
Romain Baugue <romain.baugue@elwinar.com>
2020-08-01 18:17:51 +0200

    fish: base to basic prompt

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,19 +1,12 @@
 function fish_prompt
-	set_color E5C07B
-	echo -n (date +"%T ")
-
-	if [ (whoami) = 'root' ]
-		set_color E06C75
-	else
-		set_color 98C379
-	end
-
 	set -l path "$PWD"
 	set -l path (string replace "$HOME" "~" "$PWD")
 	set -l path (string replace -ar '([^/])[^/]*/' '$1/' "$path")
 	echo -n "$path"
 
-	set_color 61AFEF
-	echo -n ' $ '
-	set_color normal
+	if [ (whoami) = 'root' ]
+		echo ' # '
+	else
+		echo ' $ '
+	end
 end
