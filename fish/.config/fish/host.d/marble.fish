efaa50a9de0f19e4ce172e3afe8bc295935416cd
Romain Baugue <romain.baugue@elwinar.com>
2020-11-16 15:46:51 +0100

    marble: Add envdir

--- fish/.config/fish/host.d/marble.fish
+++ fish/.config/fish/host.d/marble.fish
@@ -3,8 +3,7 @@ set -x GOPATH $HOME/.go
 add_to_path $GOPATH/bin
 set -x MOZ_ENABLE_WAYLAND 1
 
-set -x STO_STDCOMPOSEFILE  $HOME/development/synthesio/standard/docker-compose.yml
-set -x STO_STDPATH $HOME/development/synthesio/standard
-set -x STO_STDDIRNAME standard
+# Synthesio-related env.
 set -x AGENT_NAPOLEON_URL http://127.0.0.1
 set -x AGENT_DEVELOPMENT true
+set -x ENVDIR $HOME/development/synthesio/env
