ab6cfabd440b43421f17faca5611898e56ade0e3
Romain Baugue <romain.baugue@elwinar.com>
2023-10-03 09:54:59 +0200

    update since sable

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,11 +9,28 @@ end
 set -x LANG en_US.UTF-8
 
 # Use nvim for mostly anything.
-set -x EDITOR nvim
-set -x VISUAL nvim
-set -x SUDO_EDITOR nvim
-set -x FCEDIT nvim
-# set -x MANPAGER "nvim +'set ft=man'"
+set -x EDITOR      helix
+set -x VISUAL      helix
+set -x SUDO_EDITOR helix
+set -x FCEDIT      helix
+
+# XDG default values
+set -x XDG_DATA_HOME   $HOME/.local/share
+set -x XDG_CONFIG_HOME $HOME/.config
+set -x XDG_STATE_HOME  $HOME/.local/state
+set -x XDG_CACHE_HOME  $HOME/.cache
+
+# Overrides to force some softwares to respect XDG
+# set -x WS_SHARED_CREDENTIALS_FILE $XDG_CONFIG_HOME/aws/credentials
+# set -x AWS_CONFIG_FILE $XDG_CONFIG_HOME/aws/config
+set -x HISTFILE $XDG_STATE_HOME/bash/history
+set -x DOCKER_CONFIG $XDG_CONFIG_HOME/docker
+set -x GNUPGHOME $XDG_DATA_HOME/gnupg
+set -x LESSHISTFILE $XDG_CACHE_HOME/less/history
+alias mitmproxy="mitmproxy --set confdir=$XDG_CONFIG_HOME/mitmproxy" 
+set -x PSQL_HISTORY $XDG_DATA_HOME/psql_history
+set -x PYENV_ROOT $XDG_DATA_HOME/pyenv
+set -x VOLTA_HOME $XDG_DATA_HOME/volta
 
 # Additions to the PATH
 set -x PATH ~/.local/bin $PATH
@@ -21,13 +38,8 @@ set -x PATH ~/.local/bin $PATH
 # Ensure the fish shell doesn't fuckup with colors
 set fish_term24bit 1
 
-set -x PASSWORD_STORE_DIR ~/passwords
-
 # Import the aliases
 . (dirname (status -f))/aliases.fish
-
-# Import the colors
-# . (dirname (status -f))/colors.fish
 
 # Find and source a per-host configuraiton file
 if test -e (dirname (status -f))/host.d/(hostname).fish
--- fish/.config/fish/host.d/sable.fish
+++ fish/.config/fish/host.d/sable.fish
@@ -1,7 +1,9 @@
-set -x GOPATH $HOME/.go
+set -x GOPATH $XDG_DATA_HOME/go
 set -x GO111MODULE on
-add_to_path $HOME/.go/bin
+add_to_path $GOPATH/bin
 set -x GOPRIVATE github.com/aivetech
-add_to_path $HOME/.npm/bin
-set -x AWS_PROFILE staging
 add_to_path $HOME/bin
+
+set -x KUBECTL_EXTERNAL_DIFF kubectl-neat-diff
+set -x AWS_PROFILE staging
+set -x TOKEN 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
